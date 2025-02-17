3b4775def95cb2a8cd8327b6ad15e9babe84561a
kishanrajdev <kishanrajdev047@gmail.com>
2025-02-17 00:26:06 -0600

    custom delimiter test

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -8,5 +8,11 @@ describe('Calculator', () => {
             const output = [1, 2, 3, 4, 5, 6];
             expect(stringInputToArr(inputStr)).toEqual(output);
         });
+
+        test('converts string to array with custom delimiter specified using "//"', () => {
+            const inputStr = "//;\n1;2;3;4"
+            const output = [1, 2, 3, 4];
+            expect(stringInputToArr(inputStr)).toEqual(output);
+        });
     });
 });
