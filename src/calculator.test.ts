a90015d20d755f3bb2ea1625bc7091583cfd25f2
kishanrajdev <kishanrajdev047@gmail.com>
2025-02-17 02:03:08 -0600

    unit tests and complete code

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -3,16 +3,56 @@ import {describe} from "node:test";
 
 describe('Calculator', () => {
     describe('stringInputToArr', () => {
-        test('converts string to array with default delimiter ","', () => {
+        it('converts string to array with default delimiter ","', () => {
             const inputStr = "1, 2, 3, 4, 5, 6";
             const output = [1, 2, 3, 4, 5, 6];
             expect(stringInputToArr(inputStr)).toEqual(output);
         });
 
-        test('converts string to array with custom delimiter specified using "//"', () => {
-            const inputStr = "//;\n1;2;3;4"
-            const output = [1, 2, 3, 4];
-            expect(stringInputToArr(inputStr)).toEqual(output);
+        it('converts string to array with custom delimiter specified using "//"', () => {
+            const output = [1, 2, 3];
+            expect(stringInputToArr("//;\n1;2;3")).toEqual(output);
+            expect(stringInputToArr("//::\n1::2::3")).toEqual(output);
+            expect(stringInputToArr("//;\n1\n2;3")).toEqual(output);
+        });
+
+        it('converts string to array with \\n between numbers', () => {
+            const output = [1, 2, 3];
+            expect(stringInputToArr("1\n2,3")).toEqual(output);
+            expect(stringInputToArr("//;\n1\n2;3")).toEqual(output);
+            expect(stringInputToArr("\n1\n2,3")).toEqual([0, ...output]);
+        });
+
+        it('if with // string does not contain \\n it throws error as invalid input', () => {
+            const inputStr = "//;1;2;3;4"
+            expect(() => stringInputToArr(inputStr)).toThrow('invalid input');
+        });
+
+        it('if string contains -ve number, it throws error as invalid input', () => {
+            expect(() => stringInputToArr("1, -2, 3")).toThrow('invalid input  -2');
         });
     });
+
+    describe('add', () => {
+        it('returns 0 for empty string', () => {
+            expect(add('')).toEqual(0);
+        });
+
+        it('returns the same number for string with 1 number', () => {
+            expect(add('1')).toEqual(1);
+            expect(add('\n1\n')).toEqual(1);
+        });
+
+        it('considers empty string between delimiters as 0', () => {
+            expect(add('1,,3,')).toEqual(4);
+        });
+
+        it('considers \\n as delimiter to add numbers', () => {
+            expect(add("1\n2,3\n4")).toEqual(10);
+        });
+
+        it('adds for custom delimiter specified using "//"', () => {
+            expect(add("//::\n1::2\n3::4")).toEqual(10);
+        })
+    })
 });
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,17 +1,31 @@
 export function add (input: string): number {
-    console.log('add - ', input.indexOf('\n'));
-    return 2;
+    const inputArr = stringInputToArr(input);
+    let total = 0;
+    for (const num of inputArr) {
+        total += num;
+    }
+    return total;
 }
 
 export function stringInputToArr(input: string): number[] {
     let delimiter = ','
     if (input.substring(0, 2) === '//') {
         const inputArr = input.split('\n');
+        if (inputArr.length < 2) {
+            throw new Error('invalid input');
+        }
         const delimiterStr = inputArr[0];
         delimiter = delimiterStr.substring(2);
         input = inputArr.slice(1).join(delimiter);
     }
-    const output: number[] = input.split(delimiter).map(input => parseInt(input.trim()) || 0);
-    return output;
+    input = input.replace(/\n/g, delimiter);
+    return input.split(delimiter).map(input => {
+        const inputTrimmed = input.trim();
+        const num = inputTrimmed === "" ? 0 : parseInt(inputTrimmed);
+        if(isNaN(num) || num < 0) {
+           throw new Error('invalid input '+ input);
+       }
+       return num;
+    });
 }
 
