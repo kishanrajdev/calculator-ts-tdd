34af3a75020e1071055bcb70bb5a6181deb67560
kishanrajdev <kishanrajdev047@gmail.com>
2025-02-17 00:02:11 -0600

    adding first test

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,5 +1,16 @@
-import { add } from './calculator';
+import {add, stringInputToArr} from './calculator';
+import {describe} from "node:test";
 
 test('adds two numbers', () => {
     expect(add('2')).toBe(5);
 });
+
+describe('Calculator', () => {
+    describe('stringInputToArr', () => {
+        it('converts string to array with default delimiter ","', () => {
+            const inputStr = "1, 2, 3, 4, 5, 6";
+            const output = [1, 2, 3, 4, 5, 6];
+            expect(stringInputToArr(inputStr)).toBe(output);
+        })
+    })
+})
--- src/calculator.ts
+++ src/calculator.ts
@@ -2,4 +2,7 @@ export function add (input: string): number {
     return 2;
 }
 
+export function stringInputToArr(input: string): number[] {
+    return []
+}
 
