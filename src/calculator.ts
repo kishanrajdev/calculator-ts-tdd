4ec32d78ae4e972b71138aac66e1b6c32575cc21
kishanrajdev <kishanrajdev047@gmail.com>
2025-02-17 00:47:31 -0600

    code for custom delimiter

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,9 +1,17 @@
 export function add (input: string): number {
+    console.log('add - ', input.indexOf('\n'));
     return 2;
 }
 
 export function stringInputToArr(input: string): number[] {
-    const output: number[] = input.split(',').map(input => parseInt(input.trim()) || 0);
+    let delimiter = ','
+    if (input.substring(0, 2) === '//') {
+        const inputArr = input.split('\n');
+        const delimiterStr = inputArr[0];
+        delimiter = delimiterStr.substring(2);
+        input = inputArr.slice(1).join(delimiter);
+    }
+    const output: number[] = input.split(delimiter).map(input => parseInt(input.trim()) || 0);
     return output;
 }
 
